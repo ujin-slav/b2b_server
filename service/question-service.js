c5a78edcc91dc1f569a69673555aa70d4cd2e131
John <ujin_slav@mail.ru>
2021-10-22 15:44:33 +0300

    forum

--- service/question-service.js
+++ service/question-service.js
@@ -1,6 +1,7 @@
 const QuestModel = require("../models/question-model")
 const AskModel = require("../models/Ask-model")
-const UserModel =require('../models/user-model')
+const UserModel =require('../models/user-model');
+const { assertWrappingType } = require("graphql");
 
 class QuestService {
 
@@ -27,15 +28,19 @@ class QuestService {
     async getQuest(req) {
         const {id} = req.body
         
-        const questions = await QuestModel.find({Ask:id});
+        const questions = await QuestModel.find({Ask:id,Host:null});
         const result = await Promise.all(questions.map(async (item)=>{   
             const author = await UserModel.findOne({ _id: item.Author });
             const destination = await UserModel.findOne({ _id: item.Destination });
-            
+            const answer =  await QuestModel.find({Host:item._id});
+            const answerText = answer.map((item)=>item.Text)
             const newitem = {
+                ID: item._id,
                 Text:item?.Text,
                 Author: author?.email,
-                Destination: destination?.email
+                Destination: destination?.email,
+                DestinationID: destination?._id,
+                Answer:answerText
             }
             return newitem;
         }));
