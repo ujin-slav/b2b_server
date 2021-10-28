43ada8716bf7413b0ba290323587a47316ce1299
John <ujin_slav@mail.ru>
2021-10-28 16:45:27 +0300

    socket

--- SocketIO/index.js
+++ SocketIO/index.js
@@ -1,13 +1,19 @@
-const IOconnect = (socket) => {
-    console.log(`User Connected: ${socket.id}`);
-  
-    // socket.on("send_message", (data) => {
-    //   socket.to(data.room).emit("receive_message", data);
-    // });
-  
-    socket.on("disconnect", () => {
-      console.log("User Disconnected", socket.id);
-    });
-  };
+const IOconnect = (socket) =>{
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+    console.log(data)
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", socket.id);
+  });
+};
   
 module.exports = IOconnect;
